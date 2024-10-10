5a277edfbb517d8e5932dadbe03c2815cdf954ed
Nick Latcham <nick.latcham@proton.me>
2024-10-10 10:19:02 -0500

    feat: check and create data dirs

--- src/nnotes.nim
+++ src/nnotes.nim
@@ -1,4 +1,11 @@
 import cli/cmd
+import std/os
 
 when isMainModule:
-  echo cmd.help
+  let dataDirCheck = existsOrCreateDir(getDataDir() & "nnotes")
+  let noteBookDirCheck = existsOrCreateDir(getDataDir() & "nnotes/notebooks")
+
+  if not dataDirCheck: echo "Created \"$XDG_DATA_HOME/nnotes\" directory!"
+  if not noteBookDirCheck: echo "Created notebook dir!"
+
+  echo parseCmdArgs(commandLineParams())
