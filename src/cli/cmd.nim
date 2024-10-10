b9e88be649e222e370644853f3495290cb67c3f3
Nick Latcham <nick.latcham@proton.me>
2024-10-10 10:18:32 -0500

    feat: base cmd's

--- src/cli/cmd.nim
+++ src/cli/cmd.nim
@@ -1,3 +1,8 @@
+import ".."/persistence/models
+import std/[syncio, strformat, os, streams, strutils, sequtils]
+
+const dataDir = getDataDir() & "nnotes/notebooks"
+
 const help*: string = """
 
 NNOTES
@@ -13,16 +18,52 @@ Example:
 > $ nnotes add "rm -rf can ruin your day!" Linux
 > $ Note 1 added to "Linux" notebook!
 
-Commands:
-new       <notebook-name> <note*>  Creates a new notebook with the given name
-add       <note> <notebook>       Adds a note to an existing notebook
-list            -                 List all notebook and number of notes in each
-updaten   <note-id> <notebook>    Updates a note in a notebook
-updateb   <notbook>               Updates a notebook name
-deln      <note-id> <notebook>    Deletes a note from a notebook
-delb      <notebook>              Deletes a notebook
-help                              Displays this message
-
-* denotes an optional argument
+Commands    Args                      Description
+new         <notebook-name> <note*>   Creates a new notebook with the given name
+add         <note> <notebook>         Adds a note to an existing notebook
+list            -                     List all notebook and number of notes in each
+updaten     <note-id> <notebook>      Updates a note in a notebook
+updateb     <notbook>                 Updates a notebook name
+deln        <note-id> <notebook>      Deletes a note from a notebook
+delb        <notebook>                Deletes a notebook
+help                                  Displays this message
+-- * denotes an optional argument --
 """
 
+proc newCmd*(args: seq[string]): string =
+  writeFile(&"{dataDir}/{args[1]}.txt", "")
+
+# addCmd
+proc addCmd*(args: seq[string]): string =
+  let fstrm = newFileStream(&"{dataDir}/{args[2]}.txt", fmAppend)
+  fstrm.writeLine(args[1])
+  fstrm.close()
+  &"Added {args[1]} to {args[2]}!"
+
+proc listCmd*(args: seq[string]): string =
+  var lines = readFile(&"{dataDir}/{args[1]}.txt").split("\n").mapIt(Note(content: it))
+  $Notebook(title: args[1], notes: lines[0..^2], count: len(lines) - 1)
+
+# listCmd
+# updatenCmd
+# updatebCmd
+# delnCmd
+# delbCmd
+
+proc parseCmdArgs*(args: seq[string]): string =
+  try:
+    if args.len == 0:
+      help
+    else:
+      case args[0]:
+        of "new": $newCmd(args)
+        of "add": $addCmd(args)
+        of "list": $listCmd(args)
+        of "updaten": "TODO!"
+        of "updateb": "TODO!"
+        of "deln": "TODO!"
+        of "delb": "TODO!"
+        else: help
+  except OSError as osErr:
+    echo osErr.getStackTrace()
+    "errror"
